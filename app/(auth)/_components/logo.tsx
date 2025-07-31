0a28d4c43224c12f9db07afb1aaec372a2da07e7
sanjanavarade <127649474+sanjanavarade@users.noreply.github.com>
2025-08-01 00:03:22 +0530

    feat: integrate Clerk for authentication and add theme provider
    - Implemented ClerkProvider for user authentication.
    - Added ThemeProvider for managing themes.
    - Updated layout to include sign-in and sign-up buttons.
    - Modified Home page to display a message for authenticated users only.
    
    refactor: replace Geist font with Inter font
    
    - Removed Geist and Geist_Mono font imports.
    - Updated font usage in the layout.
    
    chore: add utility components and configurations
    
    - Created button component with variants using class-variance-authority.
    - Added utility functions for class name merging.
    - Introduced a JSON configuration for components.
    
    fix: set up middleware for route protection
    
    - Added middleware to protect routes except for sign-in pages.
    
    chore: update package dependencies
    
    - Added new dependencies for Clerk, Radix UI, and utility libraries.
    - Updated package.json and package-lock.json accordingly.
    
    style: add logo image to public directory

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import { Poppins } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
+
+const font = Poppins({
+  subsets: ['latin'],
+    weight: ['200', '300', '400', '500','600', '700', '800', '900' ],
+});
+
+export const Logo = () => {
+    return (
+        <div className={"flex flex-col items-center gap-y-4"}>
+            <div className='bg-white rounded-full p-1'>
+            <Image
+                src="/nexora logo.png"
+                alt="Nexora"
+                width="80"
+                height="80"
+                />
+            </div>
+            <div className={cn('flex flex-col items-center', font.className)}>
+                <p className="text-xl font-semibold">
+                    Nexora
+
+                </p>
+                <p className="text-sm text-muted-foreground">
+                    Let's Play
+                </p>
+            </div>
+        
+        </div>
+    );
+ }
