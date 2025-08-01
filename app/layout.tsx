dbda8b5b64d69c76184d7ec1d1c8cd280102fc62
sanjanavarade <127649474+sanjanavarade@users.noreply.github.com>
2025-08-01 23:22:11 +0530

    feat: Implement Clerk webhook for Prisma user synchronization
    This commit introduces a new API endpoint (`/api/webhooks/clerk`) to handle incoming webhooks from Clerk.
    
    - Verifies webhook signatures using the Svix library for security.
    - Handles `user.created`, `user.updated`, and `user.deleted` events.
    - Creates, updates, or deletes corresponding user records in the Prisma database to maintain data consistency between Clerk and the application.

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,16 +35,20 @@ export default function RootLayout({
           >
         <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
-              <SignInButton />
-              <SignUpButton>
+              <SignInButton >
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign In
+                </button>
+              </SignInButton>
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+              {/* <SignUpButton>
                 <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                   Sign Up
                 </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+              </SignUpButton> */}
           </header>
           
         {children}
