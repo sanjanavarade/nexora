16e88a3af5be55c84e87df06fa8f91191675745e
sanjanavarade <127649474+sanjanavarade@users.noreply.github.com>
2025-08-28 15:55:13 +0530

    feat: implement chat functionality with chat form, list, and header; add chat toggle and variant toggle components

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -1,9 +1,13 @@
 "use client"
 
 import { Stream, User } from "@/app/generated/prisma/client";
-import {LiveKitRoom} from "@livekit/components-react";//1.4.2
+import {ChatToggle, LiveKitRoom} from "@livekit/components-react";//1.4.2
+
+import { useChatSidebar } from "@/store/use-chat-sidebar";
+import { cn } from "@/lib/utils";
 import { useViewerToken } from "@/hooks/use-viewer-token";
 import { Video } from "./video";
+import { Chat } from "./chat";
 
 interface StreamPlayerProps{
   user: User & {stream: Stream | null };
@@ -20,6 +24,8 @@ const StreamPlayer = ({
     name,
     identity
   } = useViewerToken(user.id);
+
+  const { collapsed } = useChatSidebar((state)=> state)
   console.log({token, name, identity});
 
   if(!token || !name || !identity)
@@ -33,10 +39,19 @@ const StreamPlayer = ({
 
   return (
     <>
+    {
+      collapsed && (
+        <div className="hidden lg:block fixed top-[100px] right-2 z-50">
+          <ChatToggle/>
+        </div>
+      )
+    }
       <LiveKitRoom
         token = {token}
         serverUrl = {process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
-        className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+        className={cn("grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",
+          collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
+        )}
       >
         <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
           <Video
@@ -46,7 +61,22 @@ const StreamPlayer = ({
          
 
         </div>
+        <div
+          className={cn("col-span-1",
+            collapsed && "hidden"
+          )}
+        >
+          <Chat
+            viewerName ={name}
+            hostName={user.username}
+            hostIdentity={user.id}
+            isFollowing={isFollowing}
+            isChatEnabled={stream.isChatEnabled}
+            isChatDelayed={stream.isChatDelayed}
+            isChatFollowersOnly = {stream.isChatFollowersOnly}
+          />
 
+        </div>
       </LiveKitRoom>
     </>
   )
