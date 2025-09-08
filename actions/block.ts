70b73f344212ea1cb57cd2fcf893f8bc4e3d8fe7
sanjanavarade <127649474+sanjanavarade@users.noreply.github.com>
2025-09-08 19:50:57 +0530

    feat: enhance user blocking functionality and add error/not found pages

--- actions/block.ts
+++ actions/block.ts
@@ -1,14 +1,32 @@
 "use server"
 
 import { blockUser, unblockUser } from "@/lib/block-service";
+import { RoomServiceClient } from "livekit-server-sdk";
 import { revalidatePath } from "next/cache";
 
-export const onBlock = async (username: string) => {
-  const blockedUser = await blockUser(username);
-  revalidatePath("/");
-  if (blockedUser) {
-    revalidatePath(`/${blockedUser.blocked.username}`);
+
+const roomService = new RoomServiceClient(
+  process.env.LIVEKIT_API_URL!,   // ✅ Base URL (https://...)
+  process.env.LIVEKIT_API_KEY!,   // ✅ API Key
+  process.env.LIVEKIT_API_SECRET! // ✅ API Secret
+);
+
+export const onBlock = async (id: string) => {
+  const self= await blockUser(id);
+  let blockedUser;
+
+  try{
+  blockedUser = await blockUser(id);
+  }catch{
+
   }
+
+  try{
+    await roomService.removeParticipant(self.id, id);
+  }catch{
+
+  }
+  revalidatePath(`/u/${self.blocked.username}/community`);
   return blockedUser;
 };
 
--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -3,6 +3,7 @@ import { getUserByUsername } from "@/lib/user-service";
 import { notFound } from "next/navigation";
 import { Actions } from "./_components/actions";
 import { isBlockedByUser } from "@/lib/block-service";
+import StreamPlayer from "@/components/stream-player";
 
 interface UserPageProps {
     params: Promise<{
@@ -16,23 +17,24 @@ const UserPage = async ({ params }: UserPageProps) => {
 
     const user = await getUserByUsername(username);
 
-    if (!user) {
+    if (!user || !user.stream) {
         notFound();
     }
 
     const isFollowing = await isFollowingUser(user.id);
     const isBlocked = await isBlockedByUser(user.id);
 
+    if(isBlocked){
+        notFound();
+    }
+
     return (
-        <div className="flex flex-col gap-y-4">
-            <p>username: {user.username}</p>
-            <p>user ID: {user.id}</p>
-            <p>is following: {`${isFollowing}`}</p>
-            <p>is blocked by this user: {`${isBlocked}`}</p>
-            
-            <Actions  isFollowing={isFollowing} userId={user.id} username={user.username} />
-            
-        </div>
+        <StreamPlayer
+          user= {user}
+          stream={user.stream}
+          isFollowing={isFollowing}
+        
+        />
     );
 };
 
